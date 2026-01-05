bced25105f6cdf2fc0993717b53f35a35411f365
farsanathasni <thasnifarsana294@gmail.com>
2026-01-05 11:04:12 +0530

    Update Axios.jsx

--- src/Api/Axios.jsx
+++ src/Api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // Make sure this matches your JSON Server port
+  baseURL: 'https://liyana-metals-backend-1.onrender.com/', 
   timeout: 10000,
 });
 
